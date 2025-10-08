05fc78b0d5fbb41db735726f7f240408a62d79ee
Masarwa12 <167571577+Masarwa12@users.noreply.github.com>
2025-10-08 18:34:03 +0300

    Server For App Eletric-car

---
+++ SERVER/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv";
+import { connectToDB } from "../DB/db.js";
+
+import userRouter from "./users/user.router.js";
+import stationRouter from "./stations/station.router.js";
+import vehicleRouter from "./vehicles/vehicle.router.js";
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5500;
+const server = express();
+
+server.use(express.json());
+
+server.get('/',(req,res)=>{
+    res.send('Server is running and connected to DB');
+});
+
+server.use('/api/user',userRouter)
+server.use('/api/station',stationRouter)
+server.use('/api/vehicle',vehicleRouter)
+
+connectToDB().then(() => {
+    console.log("Database connected");
+    server.listen(PORT , ()=>console.log(`http://localhost:${PORT}`));
+});
---
+++ SERVER/users/user.router.js
@@ -0,0 +1,20 @@
+import {Router} from 'express'
+import { getAllUsers , getUserById , createUser , updateUser , deleteUser,Login,addVehicle,AddCard,RemoveCard, AddHistory,DeleteVehicle,AddUserChat } from './user.controller.js';
+
+const userRouter = Router();
+userRouter
+   .get('/',getAllUsers)
+   .get('/profile/:id',getUserById)
+   .post('/creditCard/:id',AddCard)
+   .post('/search_history/:id',AddHistory)
+   .post('/login',Login)
+   .post('/',createUser)
+   .post('/Vehicle/:id',addVehicle)
+   .post('/chat/:id',AddUserChat)
+   .put('/:id',updateUser)
+   .delete('/:id',deleteUser)
+   .delete('/credit_card/:id',RemoveCard)
+   .delete('/Vehicle/:id/:vehicle',DeleteVehicle)
+   
+   
+export default userRouter;
